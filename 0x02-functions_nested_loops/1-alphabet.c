4301fcd5535dd2edb5e9cfa3218d008fe468e2b3
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-03-17 05:17:10 +0100

    task 1 to 5

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet- Print lower case alphabets
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
